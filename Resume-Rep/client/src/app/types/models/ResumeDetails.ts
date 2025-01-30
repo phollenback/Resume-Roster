1b2ad075487baca1f32831f936b70b47011afe28
Peyton Hollenback <145724342+phollenback@users.noreply.github.com>
2025-01-29 17:31:12 -0800

    Delete Resume-Rep/client directory

--- Resume-Rep/client/src/app/types/models/ResumeDetails.ts
+++
@@ -1,8 +0,0 @@
-export default interface ResumeDetails {
-    userId: number;
-    positionTitle: string;
-    company: string;
-    keywords: string[];
-    date: Date;
-    file? : File | null;
-}
